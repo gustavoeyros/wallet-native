9ca1372ab2a90a5d51541ab8dacf43cb9e067967
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-06-24 19:16:50 -0300

    feat: welcome screen

---
+++ App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from "react";
+import Welcome from "./screens/Welcome";
+
+function App(): JSX.Element {
+  return <Welcome />;
+}
+
+export default App;
---
+++ components/Texts/BigText.tsx
@@ -0,0 +1,24 @@
+import React, { FunctionComponent } from "react";
+import { styled } from "styled-components/native";
+import { colors } from "../colors";
+
+import { ReactNode } from "react";
+import { StyleProp, TextStyle } from "react-native";
+
+interface TextProps {
+  textStyles?: StyleProp<TextStyle>;
+  children: ReactNode;
+}
+
+const StyledText = styled.Text`
+  font-size: 37px;
+  color: ${colors.gray};
+  text-align: left;
+  font-family: "Lato-bold";
+`;
+
+const BigText: FunctionComponent<TextProps> = (props) => {
+  return <StyledText style={props.textStyles}>{props.children}</StyledText>;
+};
+
+export default BigText;
